b399c027cf8830cf18d2220cee7a80382dd1d3cc
Siddhesh Pawar <siddheshpawar2207@gmail.com>
2026-08-06 13:25:19 +0530

    Improve RDS Terraform module

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "environment" {
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 variable "vpc_cidr" {
