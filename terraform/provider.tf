a3f05a0d5aa02ba7cee11e901cd81ce866720fbe
Siddhesh Pawar <siddheshpawar2207@gmail.com>
2026-08-06 13:05:43 +0530

    Improve RDS Terraform module

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,12 +7,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket = "your-terraform-state-bucket"
-    key    = "enterprise-python-cicd-platform/terraform.tfstate"
-    region = "ap-south-1"
-  }
 }
 
 provider "aws" {
