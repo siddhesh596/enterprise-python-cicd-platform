eff516eddc9acb986c638ad1edb15f608a1e00f6
Siddhesh Pawar <siddheshpawar2207@gmail.com>
2026-08-06 13:15:53 +0530

    Improve RDS Terraform module

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -11,7 +11,7 @@ resource "aws_lb" "main" {
 }
 
 resource "aws_lb_target_group" "main" {
-  name     = "${var.project_name}-${var.environment}-tg"
+  name = "epcp-alb-sg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
